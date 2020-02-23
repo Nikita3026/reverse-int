c6981d076bc47881b22767facc9c4871bd2587fb
Nikita Mihadyuk <mihadyuk.nikita@gmail.com>
2020-02-24 00:38:45 +0300

    feat:add function to reverse numbers in index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  if(n<0) n=-n;
+  let string=n.toString();
+  let array=string.split("");
+  array.reverse();
+  string=array.join("");
+  return +string;
 }
